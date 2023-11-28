aac168247dd3d1f5162e0307c90af4415eef793a
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2023-11-28 11:46:59 +0100

    Fix: made code shorter and clearer
    also added WeaponStore

--- 1Player/Program.cs
+++ 1Player/Program.cs
@@ -44,35 +44,47 @@ namespace _1Player
             int rope1 = 0;
             int mana1 = 0;
 
-            Console.WriteLine("You are playing single player");
-            Console.WriteLine("Do you want to start the game?");
-            input = Console.ReadLine();
-            input.ToLower();
-            if (input == "yes")
-            {
-                Console.Clear();
-                Player1Menu();
-            }
-            else
-            {
-                Console.WriteLine("So no games :,(");
-            }
+            int sword = 0;
+            int axe = 0;
+            int bow = 0;
+
+            /*monsters
+             * 
+             * Dragon (final boss) has 50hp
+             * goblin (2) 25hp
+             * Skeleton (3) 15hp
+             */
+
+            //Console.WriteLine("You are playing single player");
+            //Console.WriteLine("Do you want to start the game?");
+            //input = Console.ReadLine();
+            //input.ToLower();
+            //if (input == "yes")
+            //{
+            //    Console.Clear();
+            //    Player1Menu();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("So no games :,(");
+            //}
+
             #region player 
+            Player1Menu();
             void Player1Menu()//the menu for p1 
             {
-                //Console.Clear();
-                
+                Console.Clear();
+
                 Console.WriteLine("What do you want to do?");
                 Console.WriteLine("1 : Add resources");
                 Console.WriteLine("2 : Look at resources");// more to be added here
                 Console.WriteLine("3 : Buy weapons");
-                Console.WriteLine("4 : Attack monster");
+                Console.WriteLine("4 : Look at weapons");
+                Console.WriteLine("5 : Attack monster");
+                Console.WriteLine("6 : Quit game");
                 input = Console.ReadLine();
-                if (input == "1" || input == "2" || input == "3" || input == "4")
+                if (int.TryParse(input, out int menup1) == true)
                 {
-                    int menup1 = int.Parse(input);
-
-
                     switch (menup1)
 
                     {
@@ -82,23 +94,27 @@ namespace _1Player
                             break;
                         case 2:
 
-                            Console.WriteLine("2 option yet to be added");
+                            ResourceManagement();
                             break;
                         case 3:
-
-                            Console.WriteLine("3 option yet to be added");
+                            Console.Clear();
+                            WeaponStore();
                             break;
-
+                        case 4:
+                            WeaponLook();
+                            break;
+                        case 5:
+                            Console.WriteLine("4 option yet to be added");
+                            break;
+                        case 6:
+                            Console.WriteLine("Bye Bye :)");
+                            break;
                         default:
 
-                            if (menup1 < 3)
-                            {
-
-                            }
+                            if (menup1 < 7) { }
                             else
                             {
                                 Console.Clear();
-                                Console.WriteLine("That wasn't a option, please try again");
                                 Player1Menu();
                             }
                             break;
@@ -120,48 +136,140 @@ namespace _1Player
                 {
                     //default; will always do it
                     case 1:
-                        Console.WriteLine($"You got {howMuch} wood");
+                        Console.WriteLine($"+ {howMuch} wood");
                         wood1 += howMuch;
-                        Console.WriteLine($"You now have {wood1} wood");
+                        Console.WriteLine($"Total: {wood1} wood");
                         break;
                     case 2:
-                        Console.WriteLine($"You got {howMuch} iron");
+                        Console.WriteLine($"+ {howMuch} iron");
                         iron1 += howMuch;
-                        Console.WriteLine($"You now have {iron1} iron");
+                        Console.WriteLine($"Total: {iron1} iron");
                         break;
                     case 3:
-                        Console.WriteLine($"You got {howMuch} leather");
+                        Console.WriteLine($"+ {howMuch} leather");
                         leather1 += howMuch;
-                        Console.WriteLine($"You now have {leather1} leather");
+                        Console.WriteLine($"Total: {leather1} leather");
                         break;
                     case 4:
-                        Console.WriteLine($"You got {howMuch} stone");
+                        Console.WriteLine($"+ {howMuch} stone");
                         stone1 += howMuch;
-                        Console.WriteLine($"You now have {stone1} stone");
+                        Console.WriteLine($"Total: {stone1} stone");
                         break;
                     case 5:
-                        Console.WriteLine($"You got {howMuch} fabric");
+                        Console.WriteLine($"+ {howMuch} fabric");
                         fabric1 += howMuch;
-                        Console.WriteLine($"You now have {fabric1} fabric");
+                        Console.WriteLine($"Total: {fabric1} fabric");
                         break;
                     case 6:
-                        Console.WriteLine($"You got {howMuch} rope");
+                        Console.WriteLine($"+ {howMuch} rope");
                         rope1 += howMuch;
-                        Console.WriteLine($"You now have {rope1} rope");
+                        Console.WriteLine($"Total: {rope1} rope");
                         break;
                     case 7:
-                        Console.WriteLine($"You got {howMuch} mana");
+                        Console.WriteLine($"+ {howMuch} mana");
                         mana1 += howMuch;
-                        Console.WriteLine($"You now have {mana1} mana");
+                        Console.WriteLine($"Total: {mana1} mana");
                         break;
                 }
+
+                Console.WriteLine("Type anything to go back.");
+                Console.WriteLine();
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Player1Menu();
+                }
+                else
+                {
+                    Player1Menu();
+                }
+
+
             }
             void ResourceManagement()
             {
-
+                Console.Clear();
+                Console.WriteLine($"wood: {wood1}");
+                Console.WriteLine($"iron: {iron1}");
+                Console.WriteLine($"leather: {leather1}");
+                Console.WriteLine($"stone: {stone1}");
+                Console.WriteLine($"fabric: {fabric1}");
+                Console.WriteLine($"rope: {rope1}");
+                Console.WriteLine($"mana: {mana1}");
+                Console.WriteLine();
+                Console.WriteLine("Type anything to go back.");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Player1Menu();
+                }
+                else
+                {
+                    Player1Menu();
+                }
             }
             #endregion
-            //void 
+            void WeaponStore()
+            {
+
+
+                Console.WriteLine("Welcome to the weapon store,");
+                Console.WriteLine("you can buy weapons here from your resources.");
+                Console.WriteLine();
+                Console.WriteLine("1 : Sword, iron: 5 leather: 3");
+                Console.WriteLine("2 : Axe");
+                Console.WriteLine("3 : Bow");
+
+                Console.WriteLine("6 : Back");
+
+
+                input = Console.ReadLine();
+                if (int.TryParse(input, out int menup1) == true)
+                {
+                    if (input == "1" && iron1 > 4 && leather1 > 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You bought a sword");
+                        Console.WriteLine();
+                        Player1Menu();
+
+                    }
+                    else if (input == "6")
+                    {
+                        Player1Menu();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You are unable to do that");
+                        WeaponStore();
+                    }
+                }
+                else
+                {
+                    WeaponStore();
+                }
+
+
+                void WeaponLook()
+                {
+                    Console.WriteLine("Hello player, you have:");
+                    Console.WriteLine($"{sword} swords");
+                    Console.WriteLine($"{axe}");
+
+                    Console.WriteLine("1 : Back");
+
+
+                    input = Console.ReadLine();
+                    if (input == "1")
+                    {
+                        Console.Clear();
+                        Player1Menu();
+                    }
+                }
+
+            }
+
         }
     }
 }
---
+++ GameIdeas/Program.cs
@@ -0,0 +1,85 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Resources;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
+using System;
+
+/*dice game
+random weapon making game
+you get a random amount of (wood, iron, leather, stone, fabric, rope) and also get a random amount of mana which can make
+your weapon less durable and strong then you get to chose which weapon (bow, sword, axe, dagger, staff, shield, armor) you want to
+make and what would work best with the amount of stuf you have. you need to have a minimum of (items) to make(weapon)
+
+we need 6 dices (or we can code it but yk thats harder)
+
+minimum
+bow: wood: 5 rope: 3 stone: 3 + damage = 5 - 7 loop = roll a 6 dice and how high you get how many times you shoot arrow (damage stays the same on every arrow)
+sword: iron: 5 leather: 3 + damage = 13 - 16
+axe: iron: 6 Wood: 4 + damage = 16 - 19
+dagger: iron: 2 leather: 2 + damage = 7 - 12
+staff: wood: 4 mana: 5 + damage = 12 - 16 loop = roll a 6 dice and how high you get how many + damage you do
+shield: wood: 3 stone: 4 + block chance = 35 % (if you roll above 17) 
+armor: iron: 6 fabric: 3 + protection + 6(you get 6 less damage per attack)
+
+Bow:
+Durability 8(Chance of string breaking)
+Arrow count standard 25 (Get more arrows more mobs you kill) 
+
+Sword:
+Durability 10(Chance of blade shatter)
+
+Axe:
+Durability 9(Chance of axe flying off the handlebar)
+Upgrade AOE magic, total mana: 30(costs 1 - 6 mana per cast)
+
+Dagger:
+Durability 6(Small, cannot deal with too many stabs over time)
+
+Staff:
+Mana usage with every cast can cost 1-6 mana points, total have 30 mana points!
+
+Shield: 
+Durability 25 (After long use it'll chip away)
+
+Armour:
+Durability 20 (Can hold off slashes but does have weak spots)
+
+health = 100
+lives = 1(you can choose more if you wanna play longer, you lose everything if you die) 
+
+rules:
+resources 6 dice
+chosing resources 8 dice
+damage 20 dice
+each turn you can choose to either attack, roll a dice or make a new weapon
+if you roll under 5 you do 0 damage
+if you have a sword (13-16) and roll 18 you do 16 damage. when you roll a 20 you get +2 damage with every weapon.
+if your weapon durability runs out it breaks and you need to make a new one.
+
+mana
+(with 6 mana you can upgrade your weapons, and do 3 extra damage)
+
+1st turn: everybody rolls the dices and makes a weapon.
+2th turn: p1 attacks 
+p2 choses to roll for new resources: they roll a 8 and 6 dice, the 8 dice decides what resource you get: 
+1 wood, 2 iron, 3 leather, 4 stone, 5 fabric, 6 rope, 7 mana, 8 nothing
+and the 6 dice decides the amount of resource you get.
+
+1 wood,
+2 iron,
+3 leather,
+4 stone,
+5 fabric,
+6 rope,
+7 mana,
+8 double
+
+bow: wood: 5 rope: 3 stone: 3 + damage = 6 - 8 loop = roll a 6 dice and how high you get how many times you shoot arrow (damage stays the same on every arrow)
+sword: iron: 5 leather: 3 + damage = 13 - 16
+axe: iron: 6 Wood: 4 + damage = 16 - 19
+dagger: iron: 2 leather: 2 + damage = 7 - 12
+staff: wood: 4 mana: 5 + damage = 12 - 16 loop = roll a 6 dice and how high you get how many + damage you do
+shield: wood: 3 stone: 4 + block = if you roll above 16
+armor: iron: 6 fabric: 3 + protection + 6(you get 6 less damage per attack)
+*/
+
